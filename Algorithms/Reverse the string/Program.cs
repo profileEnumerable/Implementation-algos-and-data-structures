7092c79338f5b3f79084c7121a4906427bbbcb1a
profileEnumerable <profileenumerable@ukr.net>
2019-03-15 21:17:02 +0200

    modifying the input array(char) in-place with O(1) extra memory

---
+++ Algorithms/Reverse the string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        public static void ReverseString(char[] s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                s[i] = (char)(s[i] + s[s.Length - i - 1]);
+                s[s.Length - i - 1] = (char)(s[i] - s[s.Length - i - 1]);
+                s[i] = (char)(s[i] - s[s.Length - i - 1]);
+            }
+        }
+
+        private static void Main()
+        {
+            char[] testString = new char[] { 'h', 'e', 'l', 'l', 'o' };
+
+            Console.WriteLine($"Input: ['{ string.Join("' '", testString)}']");
+            ReverseString(testString);
+            Console.WriteLine($"Output: ['{ string.Join("' '", testString)}']");
+            Console.ReadKey();
+        }
+    }
+}
