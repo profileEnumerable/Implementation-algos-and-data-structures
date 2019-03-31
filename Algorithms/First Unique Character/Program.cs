9c355e5ea84c1d3171cd8d01d515487e15d7f301
profileEnumerable <profileenumerable@ukr.net>
2019-03-31 19:40:04 +0300

    Find first Unique Character in a String was added

---
+++ Algorithms/First Unique Character/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        public static int FirstUniqChar(string s)
+        {
+            int j;
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (j = 0; j < s.Length; j++)
+                {
+                    if (s[i] == s[j] && i != j)
+                        break;
+                }
+
+                if (j == s.Length)
+                    return i;
+            }
+            return -1;
+        }
+
+        private static void Main()
+        {
+            string[] testArr = new string[]
+            {
+                "substitute",
+                "scaffolding",
+                "eliminate",
+                "absentabsent"
+            };
+
+            foreach (var str in testArr)
+            {
+                Console.WriteLine($"Input str: {str,-15} | Output: {FirstUniqChar(str)}");
+            }
+        }
+    }
+}
