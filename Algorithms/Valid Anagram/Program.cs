2420fcab218290aacd51ecc0d4f7206367cbf810
profileEnumerable <profileenumerable@ukr.net>
2019-04-09 16:03:39 +0300

    [Valid Anagram] two task solution and func{ManualSort} with passed test was added

--- Algorithms/Valid Anagram/Program.cs
+++ Algorithms/Valid Anagram/Program.cs
@@ -1,49 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Algorithms
 {
+    internal delegate bool AnagramFunc(string s, string t);
+
     internal class Program
     {
-        //-----------------Time limit exceeded-------------//
-        //public static bool IsAnagram(string s, string t)
-        //{
-        //    int charIndex = 0, foundIndex = 0;
-
-        //    if (s.Length == t.Length)
-        //    {
-        //        while (charIndex < s.Length && (foundIndex = t.IndexOf(s[charIndex])) != -1)
-        //        {
-        //            t = t.Remove(foundIndex, 1);                    
-        //            charIndex++;
-        //        }
-        //        return t.Length == 0;
-        //    }
-        //    return false;
-        //}
-
-        public static bool IsAnagram(string s, string t)
+        public static void ShowTestsFunc(params AnagramFunc[] funcs)
         {
-            int foundIndex = 0;
+            Stopwatch stopwatch = new Stopwatch();
+
+            Dictionary<string, string> testData = new Dictionary<string, string>()
+            {
+                { "anagram","nagaram" },
+                { "rat","car" }
+            };
+
+            foreach (KeyValuePair<string, string> data in testData)
+            {
+                Console.WriteLine($"Test data: {data}\n");
+
+                foreach (AnagramFunc func in funcs)
+                {
+                    stopwatch.Restart();
+                    Console.Write($"{func.Method.Name,-25} |Result: {func(data.Key, data.Value),-10} ");
+                    stopwatch.Stop();
+                    Console.WriteLine("|ElapseTicks: " + stopwatch.ElapsedTicks);
+                }
+                Console.WriteLine(new string('-', 65));
+            }
+        }
+
+        //-----------------Time limit exceeded-------------//
+        public static bool IsAnagramDeletingElem(string s, string t)
+        {
+            int charIndex = 0;
 
             if (s.Length == t.Length)
             {
-                for (int i = 0; i < s.Length; i++)
+                int foundIndex;
+                while (charIndex < s.Length && (foundIndex = t.IndexOf(s[charIndex])) != -1)
                 {
-                    if (s[i] != t[i] && (foundIndex = t.IndexOf(s[i], i + 1)) == -1)//optimaze find with start index 
-                        break;
-
-                    else if (s[i] != t[i])
-                    {
-                        //TODO:Swap the symbols                        
-                    }
+                    t = t.Remove(foundIndex, 1);
+                    charIndex++;
                 }
+                return t.Length == 0;
             }
             return false;
         }
 
+        public static bool IsAnagramManualSort(string s, string t)
+        {
+            int foundIndex;
+            char[] tArr = t.ToCharArray();
+
+            if (s.Length != t.Length)
+                return false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != tArr[i])
+                {
+                    if ((foundIndex = Array.IndexOf(tArr, s[i], i + 1)) == -1)
+                        return false;
+
+                    char temp = tArr[i];
+                    tArr[i] = tArr[foundIndex];
+                    tArr[foundIndex] = temp;
+                }
+            }
+            return true;
+        }
+
         private static void Main()
         {
-            Console.WriteLine(IsAnagram("anagram", "nagaram"));
+            ShowTestsFunc(IsAnagramDeletingElem, IsAnagramManualSort);
         }
     }
 }
