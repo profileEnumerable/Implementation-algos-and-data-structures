7939558ca0f1e05ce78d00cb12fb436a0878ec1e
profileEnumerable <profileenumerable@ukr.net>
2019-04-20 11:54:29 +0300

    [Valid Palindrome] Fast alg with the test data was added

---
+++ Algorithms/Valid Palindrome/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        public static bool IsPalindrome(string s)
+        {
+            s = s.ToLower();
+
+            int st = 0;
+            int end = s.Length - 1;
+
+            while (st < end)
+            {
+                if (s[st] != s[end])
+                {
+                    if (!char.IsLetterOrDigit(s[st]))
+                    {
+                        st++;
+                        continue;
+                    }
+                    if (!char.IsLetterOrDigit(s[end]))
+                    {
+                        end--;
+                        continue;
+                    }
+                    return false;
+                }
+                st++;
+                end--;
+            }
+            return true;
+        }
+
+        private static void Main()
+        {
+            string[] testData = new string[]
+            {
+                "a.",
+                "0P",
+                "race a car",
+                "0:u`bVbu`:0",
+                "`l;`` 1o1 ??;l`",
+            };
+
+            foreach (string str in testData)
+            {
+                Console.WriteLine($"Input: |{str + "|",-20} Output: {IsPalindrome(str)}");
+            }
+        }
+    }
+}
