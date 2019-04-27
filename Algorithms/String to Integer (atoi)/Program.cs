4e07e805523badc6efad741fa7fff5d4793971c0
profileEnumerable <profileenumerable@ukr.net>
2019-04-27 21:09:42 +0300

    [String to Integer (atoi)] ready-made alg with passed tests was added

---
+++ Algorithms/String to Integer (atoi)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Algorithms
+{
+    internal class Program     
+    {
+        public int MyAtoi(string str)
+        {
+            str = str.TrimStart();//remove all leading spaces
+
+            int stIndex = str != "" && (str[0] == '-' || str[0] == '+') ? 1 : 0;
+
+            while (stIndex < str.Length && char.IsDigit(str[stIndex]))//count the first digits
+            {
+                stIndex++;
+            }
+
+            string numberStr = str.Substring(0, stIndex);//extract potential number as string
+
+            if (!int.TryParse(numberStr, out int res) && stIndex > 1)//check for overflow
+            {
+                return numberStr[0] == '-' ? int.MinValue : int.MaxValue;
+            }
+
+            return res;
+        }
+
+        private static void Main()
+        {
+            Console.WriteLine(new Program().MyAtoi("    -45"));
+        }
+    }
+}
