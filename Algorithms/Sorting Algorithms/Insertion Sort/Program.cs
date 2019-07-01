2be44795be1441f60c818d9048e2b42e54bcd5a9
profileEnumerable <profileenumerable@ukr.net>
2019-07-01 13:46:15 +0300

    [Insertion Sort] optimized alg from ITVDN examples was added & compare with my implemantation

--- Algorithms/Sorting Algorithms/Insertion Sort/Program.cs
+++ Algorithms/Sorting Algorithms/Insertion Sort/Program.cs
@@ -10,14 +10,14 @@ namespace Algorithms
     {
         #region Test funcs on time
 
-        public static void ShowTestsFunc(SortFunc sortFuncs)
+        public static void ShowTestsFunc(params SortFunc[] sortFuncs)
         {
             var stopwatch = new Stopwatch();
 
             var testData = new Dictionary<int[], string>()
             {
-                { new[] { 31,10,1,2},"O(n^2)" },
-                //{ new[] { 1, 2, 3, 4, 5, 6,  7 },"O(n)"   },
+                { new[] { 1, 3, 2, 0, -1, 10, 4, 456 },"O(n^2)" },
+                { new[] { 1, 2, 3, 4,  5,  6, 7,   8},"O(n)" },
             };
 
             foreach (KeyValuePair<int[], string> data in testData)
@@ -25,28 +25,67 @@ namespace Algorithms
                 Console.WriteLine($"Time complexity: {data.Value}");
                 Console.WriteLine($"Before sorting : {string.Join(" ", data.Key)}");
 
-                stopwatch.Restart();
-                sortFuncs(data.Key);
-                stopwatch.Stop();
+                foreach (SortFunc sortFunc in sortFuncs)
+                {
+                    int[] cloneArr = data.Key.Clone() as int[];
 
-                Console.Write($"{sortFuncs.Method.Name,-25} |After sorting: {string.Join(" ", data.Key),-10} ");
-                Console.WriteLine("|ElapseTicks: " + stopwatch.ElapsedTicks);
+                    stopwatch.Restart();
+                    sortFunc(cloneArr);
+                    stopwatch.Stop();
+                    Console.Write($"{sortFunc.Method.Name,-25} |After sorting: {string.Join(" ", cloneArr),-10} ");
+                    Console.WriteLine("|ElapseTicks: " + stopwatch.ElapsedTicks);
+                }
 
                 Console.WriteLine(new string('-', 65));
             }
         }
         #endregion
 
-        public static int FindIndexForInsert(int[] array, int currIndex)
+        //---------Own implementation of the sorting algorithm-------//
+        public static void InsertionSortOwnImplem(int[] array)
         {
-            for (int i = currIndex - 1; i >= 0; i--)
+            for (int i = 1; i < array.Length; i++)
             {
-                if (array[currIndex] > array[i])
+                if (array[i - 1] > array[i])
                 {
-                    return i + 1;
+                    int insertionInd = i;//save current index
+                    int insertionElem = array[i];//save insertion item
+
+                    while (insertionInd != 0 && insertionElem < array[insertionInd - 1])
+                    {
+                        array[insertionInd] = array[insertionInd - 1];//offset the items
+                        insertionInd--;
+                    }
+
+                    array[insertionInd] = insertionElem;
                 }
             }
-            return 0;
+        }
+
+        //---------Optimized algorithm from ITVDN examples----------//
+        public static int FindIndexForInsert(int[] array, int insertionElem)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (insertionElem < array[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static void Insert(int[] array, int insertTo, int insertFrom)
+        {
+            int temp = array[insertTo];//save the future right side item
+            array[insertTo] = array[insertFrom];
+
+            for (int i = insertFrom; i > insertTo + 1; i--)
+            {
+                array[i] = array[i - 1];//offset the items
+            }
+
+            array[insertTo + 1] = temp;//restore the right side item
         }
 
         public static void InsertionSort(int[] array)
@@ -55,23 +94,15 @@ namespace Algorithms
             {
                 if (array[i - 1] > array[i])
                 {
-                    int tempIndex = i;
-                    int temp = array[i];
-
-                    while (tempIndex != 0 && temp < array[tempIndex - 1])
-                    {
-                        array[tempIndex] = array[tempIndex - 1];
-                        tempIndex--;
-                    }
-
-                    array[tempIndex] = temp;
+                    int insertTo = FindIndexForInsert(array, array[i]);
+                    Insert(array, insertTo, i);
                 }
             }
         }
 
         private static void Main()
         {
-            ShowTestsFunc(InsertionSort);
+            ShowTestsFunc(InsertionSort, InsertionSortOwnImplem);
         }
     }
 }
