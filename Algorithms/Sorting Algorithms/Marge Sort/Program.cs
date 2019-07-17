4bfe6c0ac752e71441ee4bc269851765c18b9390
profileEnumerable <profileenumerable@ukr.net>
2019-07-17 21:47:15 +0300

    [Merge Sort] ready-made alg with the test func was added

--- Algorithms/Sorting Algorithms/Marge Sort/Program.cs
+++ Algorithms/Sorting Algorithms/Marge Sort/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
- using System.Text;
+using System.Text;
 
 namespace Algorithms
 {
@@ -8,23 +9,63 @@ namespace Algorithms
     {
         #region Test funcs on time
 
-        public static void ShowTestsFunc(Func<int, string> func)
+        public static void ShowTestsFunc(Action<int[]> sortFunc)
         {
             var stopwatch = new Stopwatch();
 
-            int[] testData = { 1, 2, 3, 4 };
-
-            foreach (int data in testData)
+            var testData = new Dictionary<int[], string>()
             {
-                stopwatch.Restart();
-                Console.Write($"{func.Method.Name,-25} | Output: {func(data),-10} ");
-                stopwatch.Stop();
-                Console.WriteLine("|ElapseTicks: " + stopwatch.ElapsedTicks);
+                { new[] { 1, 3, 2, 0, -1, 10, 4, 456 },"O(n log n)"},
+                { new[] { 1, 2, 3, 4,  5,  6, 7,   8},"O(n log n)" },
+            };
 
-                Console.WriteLine(new string('-', 65));
+            foreach (KeyValuePair<int[], string> data in testData)
+            {
+                Console.WriteLine($"Time complexity: {data.Value}");
+                Console.WriteLine($"Before sorting : {string.Join(" ", data.Key)}");
+
+                stopwatch.Restart();
+                sortFunc(data.Key);
+                stopwatch.Stop();
+                Console.Write($"{sortFunc.Method.Name,-25} |After sorting: {string.Join(" ", data.Key),-10} ");
+                Console.WriteLine("|ElapseTicks: " + stopwatch.ElapsedTicks);
             }
+
+            Console.WriteLine(new string('-', 65));
         }
         #endregion
+
+        public static void Merge(int[] array, int[] left, int[] right)
+        {
+            int leftIndex = 0;
+            int rightIndex = 0;
+            int currentIndex = 0;
+
+            int remaining = left.Length + right.Length;//total length
+
+            while (remaining > 0)
+            {
+                if (leftIndex >= left.Length)
+                {
+                    array[currentIndex] = right[rightIndex++];
+                }
+                else if (rightIndex >= right.Length)
+                {
+                    array[currentIndex] = left[leftIndex++];
+                }
+                else if (left[leftIndex] < right[rightIndex])
+                {
+                    array[currentIndex] = left[leftIndex++];
+                }
+                else
+                {
+                    array[currentIndex] = right[rightIndex++];
+                }
+
+                currentIndex++;
+                remaining--;
+            }
+        }
 
         public static void MergeSort(int[] array)
         {
@@ -48,14 +89,12 @@ namespace Algorithms
             MergeSort(left);
             MergeSort(right);
 
+            Merge(array, left, right);
         }
 
         private static void Main()
         {
-            int[] array = { 7, 4, 3, 9, 1, 0 };
-
-            MergeSort(array);
-
+            ShowTestsFunc(MergeSort);
         }
     }
 }
