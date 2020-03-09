7995facd5363e06437ce4473467d21ebf196d6f9
profileEnumerable <fustethernet@gmail.com>
2020-03-09 22:56:20 +0200

    approximate solution without tests

---
+++ Algorithms/[LeetCode]/Longest Common Prefix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+public sealed class Test
+{
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        int currCharPosition = 0;
+        string prefix = string.Empty;
+
+
+        while (true)
+        {
+            if (currCharPosition < strs[0].Length)
+            {
+                prefix += strs[0][currCharPosition];
+            }
+
+            for (int i = 1; i < strs.Length; i++)
+            {
+                if (currCharPosition < strs[i].Length && strs[i][currCharPosition] != prefix[currCharPosition])
+                {
+                    Console.WriteLine(prefix);
+                }
+            }
+
+            currCharPosition++;
+        }
+    }
+
+    private static void Main()
+    {
+        LongestCommonPrefix(new[] {"flower", "flow", "flight"});
+    }
+}
